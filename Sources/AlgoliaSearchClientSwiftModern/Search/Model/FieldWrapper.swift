09500f82c530c076d39e0915b159515d778dd2a6
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-10-30 10:58:59 +0100

    remove redundant DynamicKey

--- Sources/AlgoliaSearchClientSwiftModern/Search/Model/FieldWrapper.swift
+++ Sources/AlgoliaSearchClientSwiftModern/Search/Model/FieldWrapper.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 internal struct FieldWrapper<Wrapped: Encodable>: Encodable {
-  
   let key: String
   let wrapped: Wrapped
 
@@ -11,14 +10,14 @@ internal struct FieldWrapper<Wrapped: Encodable>: Encodable {
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: DynamicKey.self)
-    try container.encode(wrapped, forKey: DynamicKey(stringValue: key))
+    var container = encoder.container(keyedBy: CustomCodingKey.self)
+    try container.encode(wrapped, forKey: CustomCodingKey(stringValue: key))
   }
-  
+
   static func make(_ key: String) -> (Wrapped) -> Self {
     { wrapped in FieldWrapper(key: key, wrapped) }
   }
-  
+
   static func params(_ wrapped: Wrapped) -> Self { make("params")(wrapped) }
   static func requests(_ wrapped: Wrapped) -> Self { make("requests")(wrapped) }
   static func events(_ wrapped: Wrapped) -> Self { make("events")(wrapped) }
@@ -26,5 +25,4 @@ internal struct FieldWrapper<Wrapped: Encodable>: Encodable {
   static func remove(_ wrapped: Wrapped) -> Self { make("remove")(wrapped) }
   static func cluster(_ wrapped: Wrapped) -> Self { make("cluster")(wrapped) }
   static func cursor(_ wrapped: Wrapped) -> Self { make("cursor")(wrapped) }
-  
 }
